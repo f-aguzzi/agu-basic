164b94f0f3ea6d839b91d2471e96793687309a72
Federico Aguzzi <federico.aguzzi@icloud.com>
2023-09-07 18:05:21 +0200

    Created lexer (kind of)

--- src/parser/lexer.rs
+++ src/parser/lexer.rs
@@ -1,6 +1,41 @@
 use std::str::Lines;
 
-pub fn lexer(code: String) -> Vec<String> {
+pub enum Tokens {
+    // literals:
+    LITERAL,
+    // mathematical operators:
+    PLUS,
+    MINUS,
+    TIMES,
+    DIVIDE,
+    // logical operators:
+    GREATER,
+    LESSER,
+    EQUAL,
+    GREATEREQUAL,
+    LESSEREQUAL,
+    // booleans:
+    TRUE,
+    FALSE,
+    // control flow:
+    IF,
+    ENDIF,
+    GOTO,
+    FOR,
+    WHILE,
+    // let statement:
+    LET,
+}
+
+pub struct Token {
+    tpe: Tokens,
+    text: String,
+}
+
+
+
+pub fn lexer(code: String) -> Vec<Vec<Token>> {
+    // Split lines, remove empty lines
     let lines = code.lines();
     let mut lines_vector: Vec<String> = Vec::new();
     for line in lines {
@@ -9,5 +44,49 @@ pub fn lexer(code: String) -> Vec<String> {
         }
     }
 
-    lines_vector
+    // Storage vector for tokenized lines
+    let mut tokenized_lines: Vec<Vec<Token>> = Vec::new();
+
+    for line in lines_vector {
+
+        // Temporary vector for current line tokens
+        let mut tokenized_vec: Vec<Token> = Vec::new();
+
+        let tokens: Vec<_> = line.split_whitespace().collect();
+
+        for token in tokens {
+            let tpe = match token {
+                "+" => Tokens::PLUS,
+                "-" => Tokens::MINUS,
+                "*" => Tokens::TIMES,
+                "/" => Tokens::DIVIDE,
+                ">" => Tokens::GREATER,
+                "<" => Tokens::LESSER,
+                "=" => Tokens::EQUAL,
+                ">=" => Tokens::GREATEREQUAL,
+                "<=" => Tokens::LESSEREQUAL,
+                "TRUE" => Tokens::TRUE,
+                "FALSE" => Tokens::FALSE,
+                "IF" => Tokens::IF,
+                "ENDIF" => Tokens::ENDIF,
+                "GOTO" => Tokens::GOTO,
+                "FOR" => Tokens::FOR,
+                "WHILE" => Tokens::WHILE,
+                "LET" => Tokens::LET,
+                _=> Tokens::LITERAL
+            };
+            let text: String = token.to_owned();
+            tokenized_vec.push(
+                Token {
+                    tpe,
+                    text
+                }
+            )
+        }
+
+        tokenized_lines.push(tokenized_vec);
+
+    }
+
+    tokenized_lines
 }
--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -1,3 +1,9 @@
+use crate::ast::structs::{Program, IfStatement, Logical};
+use super::lexer::{lexer, Token};
 
+pub fn parse() {
+    let mut code: Vec<Vec<Token>> = lexer("IF".to_string());
 
+    let mut program: Program;
 
+}
